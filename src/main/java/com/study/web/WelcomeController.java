a2f0f41578fd18aced2240b819e006f533a9c74a
qwebnm7788 <qwebnm7788@naver.com>
2018-10-05 15:11:36 +0900

    2-1 Complete

---
+++ src/main/java/com/study/web/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.study.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WelcomeController {
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String welcome(String name, Model model) {
+		System.out.println("name : " + name);
+		model.addAttribute("name", name);
+		return "welcome";
+	}
+}
