cf0974119bd64425d9eb2f756421c66fe4305ef2
qwebnm7788 <qwebnm7788@naver.com>
2018-10-05 15:56:01 +0900

    2-3 Complete
    Add User list page

--- src/main/java/com/study/web/UserController.java
+++ src/main/java/com/study/web/UserController.java
@@ -1,6 +1,10 @@
 package com.study.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,9 +12,18 @@ import com.study.domain.User;
 
 @Controller
 public class UserController {
+	private List<User> users = new ArrayList<>();
+	
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
 	public String create(User user) {
 		System.out.println(user);
-		return "welcome";
+		users.add(user);
+		return "redirect:/list";
+	}
+	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String list(Model model) {
+		model.addAttribute("users", users);
+		return "list";
 	}
 }
