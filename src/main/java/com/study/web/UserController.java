e25f0c357b7c506299e22124eaf7f0f1e1888480
qwebnm7788 <qwebnm7788@naver.com>
2018-10-07 14:31:44 +0900

    3-2 Add Jpa and h2 database

--- src/main/java/com/study/web/UserController.java
+++ src/main/java/com/study/web/UserController.java
@@ -1,29 +1,29 @@
 package com.study.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.study.domain.User;
+import com.study.persistence.UserRepository;
 
 @Controller
 public class UserController {
-	private List<User> users = new ArrayList<>();
+	@Autowired
+	private UserRepository userRepository;
 	
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
 	public String create(User user) {
 		System.out.println(user);
-		users.add(user);
+		userRepository.save(user);
 		return "redirect:/list";
 	}
 	
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public String list(Model model) {
-		model.addAttribute("users", users);
+		model.addAttribute("users", userRepository.findAll());
 		return "list";
 	}
 }
